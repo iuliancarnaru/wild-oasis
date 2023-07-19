79728c03ad736e8d7902d7ed347807446ef20b35
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-07-19 18:27:49 +0100

    update queries

--- src/services/apiBookings.ts
+++ src/services/apiBookings.ts
@@ -91,7 +91,7 @@ export async function getStaysTodayActivity() {
     .from("bookings")
     .select("*, guests(full_name, nationality, country_flag)")
     .or(
-      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
+      `and(status.eq.unconfirmed,start_date.eq.${getToday()}),and(status.eq.checked-in,end_date.eq.${getToday()})`
     )
     .order("created_at");
 
